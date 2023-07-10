c2a7c57a2bf6f9fe3d914d57306155baaa72aab9
Shane <innon.shen@gmail.com>
2023-07-10 15:26:47 +0800

    Test azkaban API

--- azkaban/api/request_test.go
+++ azkaban/api/request_test.go
@@ -3,19 +3,74 @@ package api
 import (
 	"context"
 	"fmt"
+	"os"
 	"testing"
 )
 
-func TestAuthenticate(t *testing.T) {
-	params := AuthenticateParams{
-		ServerUrl: "http://azkaban:20000",
-		Username:  "metrics",
-		Password:  "metrics",
-	}
-	sessionId, err := Authenticate(params, context.Background())
+const (
+	serverUrl = "http://azkaban:20000"
+	username  = "metrics"
+	password  = "metrics"
+)
+
+func TestAPI(t *testing.T) {
+	// Authenticate
+	sessionId, err := Authenticate(AuthenticateParams{
+		ServerUrl: serverUrl,
+		Username:  username,
+		Password:  password,
+	}, context.Background())
 	if err != nil {
-		fmt.Printf("%+v", err)
-		return
+		fmt.Printf("%+v\n", err)
+		os.Exit(1)
 	}
-	fmt.Printf("session id: %s\n", sessionId)
+	fmt.Printf("SESSION_ID: %s\n", sessionId)
+
+	// FetchUserProjects
+	projects, err := FetchUserProjects(FetchUserProjectsParams{
+		ServerUrl: serverUrl,
+		SessionId: sessionId,
+	}, context.Background())
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%+v\n", projects)
+
+	if len(projects) == 0 {
+		fmt.Println("No project")
+		os.Exit(1)
+	}
+
+	// FetchFlowsOfAProject
+	flows, err := FetchFlowsOfAProject(FetchFlowsOfAProjectParams{
+		ServerUrl:   serverUrl,
+		SessionId:   sessionId,
+		ProjectName: projects[0].ProjectName, // or specify manually
+	}, context.Background())
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%+v\n", flows)
+
+	if len(flows) == 0 {
+		fmt.Println("No flow")
+		os.Exit(1)
+	}
+
+	// FetchExecutionsOfAFlow
+	executions, err := FetchExecutionsOfAFlow(FetchExecutionsOfAFlowParams{
+		ServerUrl:   serverUrl,
+		SessionId:   sessionId,
+		ProjectName: projects[0].ProjectName,
+		FlowId:      flows[0].FlowId,
+		StartIndex:  0,
+		ListLength:  1,
+	}, context.Background())
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%+v\n", executions)
 }
